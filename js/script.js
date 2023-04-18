f201601e0d00efd3fea60191faa2d5340de4d1a3
murzinden <80094162+murzinden@users.noreply.github.com>
2023-04-19 02:13:12 +0400

    test js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('open-popup');
+const el = document.getElementById('pop-el');
+const closeButton = document.getElementById('pop-close')
+
+function addClass() {
+  el.classList.add('popup_opened');
+}
+function removeClass() {
+  el.classList.remove('popup_opened');
+}
+
+button.addEventListener('click', addClass);
+
+closeButton.addEventListener('click', removeClass);
